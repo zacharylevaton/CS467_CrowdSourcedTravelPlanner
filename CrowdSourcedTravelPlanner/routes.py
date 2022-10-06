227a3cf7163e7785c0cc5ecd108b26aab7421d36
Zachary Levaton <zach.levaton.0514@gmail.com>
2022-10-06 22:14:43 +0100

    Initial release of base Flask package.

---
+++ CrowdSourcedTravelPlanner/routes.py
@@ -0,0 +1,6 @@
+from CrowdSourcedTravelPlanner import app
+from flask import render_template
+
+@app.route('/')
+def landing():
+    return render_template('landing.html')
---
+++ app.py
@@ -0,0 +1,4 @@
+from CrowdSourcedTravelPlanner import app
+
+if __name__ == '__main__':
+    app.run(debug = True)
